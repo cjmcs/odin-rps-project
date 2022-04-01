6de57ec7e041e552a3a2cffe4869c635ef0dfe60
Christopher <98278053+cjmcs@users.noreply.github.com>
2022-04-01 14:42:15 +0100

    Addedfunction to loop 5 rounds of the game. Added points mechanism

--- javascript.js
+++ javascript.js
@@ -1,42 +1,54 @@
-function computerPlay() {
-    const rpsAction = ['rock', 'paper', 'scissors'];
-    console.log(rpsAction)
-    let computerSelection = rpsAction[Math.floor(Math.random() * rpsAction.length)];
-    console.log(`computer choice is ${computerSelection}`)
-    return computerSelection;
+let humanScore = 0
+let ComputerScore = 0
 
+function computerPlay() {
+  const rpsAction = ['rock', 'paper', 'scissors'];
+  console.log(rpsAction);
+  const computerSelection = rpsAction[Math.floor(Math.random() * rpsAction.length)];
+  // console.log(`computer choice is ${computerSelection}`);
+  return computerSelection;
 }
 
 function humanPlay() {
-    let humanSelection = prompt('Rock Paper or Scissors')
-    humanSelection = humanSelection.toLowerCase()
-    console.log(`human choice is ${humanSelection}`)
-    return humanSelection;
+  let humanSelection = prompt('Rock Paper or Scissors');
+  humanSelection = humanSelection.toLowerCase();
+  // console.log(`human choice is ${humanSelection}`);
+  return humanSelection;
 }
 
-const computerSelection = computerPlay()
-const humanSelection = humanPlay()
-
-
+// const computerSelection = computerPlay();
+// const humanSelection = humanPlay();
 
 function playRound() {
-    if (playRound) {
-        if (humanSelection === 'rock' && computerSelection === 'scissors' ||
-            humanSelection === 'paper' && computerSelection === 'rock' ||
-            humanSelection === 'scissors' && computerSelection === 'paper')
-            console.log(`human wins ${humanSelection} beats ${computerSelection}`)
-        else if (humanSelection === computerSelection)
-            console.log(`it's a tie!`)
-        else if (computerSelection === 'rock' && humanSelection === 'scissors' ||
-            computerSelection === 'paper' && humanSelection === 'rock' ||
-            computerSelection === 'scissors' && humanSelection === 'paper')
-            console.log(`computer wins ${computerSelection} beats ${humanSelection}`)
-    
-        else console.log(`there has been an error`)
-    }
-    // console.log(`computer selection is ${computerSelection} and human selection is ${humanSelection}`)
-
+const computerSelection = computerPlay();
+const humanSelection = humanPlay();
+  if (playRound) {
+    if (humanSelection === 'rock' && computerSelection === 'scissors' ||
+      humanSelection === 'paper' && computerSelection === 'rock' ||
+      humanSelection === 'scissors' && computerSelection === 'paper') {
+      console.log(`human wins ${humanSelection} beats ${computerSelection}`);
+    humanScore++
+    } else if (humanSelection === computerSelection) {
+      console.log(`it\'s a tie! both selected ${humanSelection}`);
+    humanScore++ && ComputerScore++
+    } else if (computerSelection === 'rock' && humanSelection === 'scissors' ||
+      computerSelection === 'paper' && humanSelection === 'rock' ||
+      computerSelection === 'scissors' && humanSelection === 'paper') {
+      console.log(`computer wins ${computerSelection} beats ${humanSelection}`);
+    ComputerScore++
+    } else console.log('there has been an error');
+  }
+  console.log(`Your score is: ${humanScore} and AI score is ${ComputerScore}`)
+  // console.log(`computer selection is ${computerSelection} and human selection is ${humanSelection}`)
 }
 
-playRound(computerSelection, humanSelection)
+// playRound(computerSelection, humanSelection);
 // console.log(`computer selection is ${computerSelection} and human selection is ${humanSelection}`)
+
+function playGame(){
+  for (let counter = 0; counter <5; counter++){
+    playRound(counter);
+  }
+}
+
+playGame()
