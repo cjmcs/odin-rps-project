715d4d868617176558856ea1148cd86d6070bc9f
Christopher <98278053+cjmcs@users.noreply.github.com>
2022-04-08 11:36:51 +0100

    Added validation rules to humanSelection Tidied up via ESLint

--- javascript.js
+++ javascript.js
@@ -1,54 +1,80 @@
-let humanScore = 0
-let ComputerScore = 0
+let humanScore = 0;
+let computerScore = 0;
+let humanSelection;
+let computerSelection;
 
-function computerPlay() {
-  const rpsAction = ['rock', 'paper', 'scissors'];
-  console.log(rpsAction);
-  const computerSelection = rpsAction[Math.floor(Math.random() * rpsAction.length)];
-  // console.log(`computer choice is ${computerSelection}`);
-  return computerSelection;
+//Function determines computers choice from an array
+function computerPlay(rpsAction, computerSelection) {
+  rpsAction = ["rock", "paper", "scissors"];
+  console.table(rpsAction); // Checks items in Array
+  computerSelection = rpsAction[Math.floor(Math.random() * rpsAction.length)]; //Chooses Selection at random
+  console.log(`computer selected ${computerSelection}`)
+  return computerSelection; //Returns Selection
 }
-
+//Function Determines humans choice from prompt question & converts to lower case
 function humanPlay() {
-  let humanSelection = prompt('Rock Paper or Scissors');
-  humanSelection = humanSelection.toLowerCase();
-  // console.log(`human choice is ${humanSelection}`);
+  humanSelection = prompt("Please choose Rock, Paper, or Scissors").toLowerCase();
+  validate(); // calls validaton function
   return humanSelection;
 }
 
-// const computerSelection = computerPlay();
-// const humanSelection = humanPlay();
 
+//Function to validate human input
+function validate() {
+  if (humanSelection === "rock" ||
+    humanSelection === "paper" ||
+    humanSelection === "scissors") {
+    console.log(`human selection is ${humanSelection}`); // Checks if human selection matches possible selections
+    return humanSelection;
+  } else if (humanSelection !== "rock" ||
+    humanSelection !== "paper" ||
+    humanSelection !== "scissors") {
+    let localHumanSelection = prompt("invalid response, try again").toLowerCase(); //If human selection does not match then reprompt for valid option
+    humanSelection = localHumanSelection;
+    validate(); //revalidates until valid option selected
+    return humanSelection;
+  }
+}
+
+//Determines game logic and assigns points to respective score
 function playRound() {
-const computerSelection = computerPlay();
-const humanSelection = humanPlay();
+  computerSelection = computerPlay(); // calls function to determine computer selection
+  humanSelection = humanPlay(); // calls function to determine human selection
   if (playRound) {
-    if (humanSelection === 'rock' && computerSelection === 'scissors' ||
-      humanSelection === 'paper' && computerSelection === 'rock' ||
-      humanSelection === 'scissors' && computerSelection === 'paper') {
-      console.log(`human wins ${humanSelection} beats ${computerSelection}`);
-    humanScore++
+    if (humanSelection === "rock" && computerSelection === "scissors" ||
+      humanSelection === "paper" && computerSelection === "rock" ||
+      humanSelection === "scissors" && computerSelection === "paper") {
+      console.log(`human wins round ${humanSelection} beats ${computerSelection}`);
+      humanScore++;
     } else if (humanSelection === computerSelection) {
-      console.log(`it\'s a tie! both selected ${humanSelection}`);
-    humanScore++ && ComputerScore++
-    } else if (computerSelection === 'rock' && humanSelection === 'scissors' ||
-      computerSelection === 'paper' && humanSelection === 'rock' ||
-      computerSelection === 'scissors' && humanSelection === 'paper') {
-      console.log(`computer wins ${computerSelection} beats ${humanSelection}`);
-    ComputerScore++
-    } else console.log('there has been an error');
+      console.log(`it's a tie! both selected ${humanSelection}`);
+      humanScore++;
+      computerScore++;
+    } else if (computerSelection === "rock" && humanSelection === "scissors" ||
+      computerSelection === "paper" && humanSelection === "rock" ||
+      computerSelection === "scissors" && humanSelection === "paper") {
+      console.log(`computer wins round ${computerSelection} beats ${humanSelection}`);
+      computerScore++;
+    } else console.log("there has been an error");
   }
-  console.log(`Your score is: ${humanScore} and AI score is ${ComputerScore}`)
-  // console.log(`computer selection is ${computerSelection} and human selection is ${humanSelection}`)
+  // console.log(`Your score is: ${humanScore} and AI score is ${computerScore}`)
 }
 
-// playRound(computerSelection, humanSelection);
-// console.log(`computer selection is ${computerSelection} and human selection is ${humanSelection}`)
-
-function playGame(){
-  for (let counter = 0; counter <5; counter++){
-    playRound(counter);
+//determines how many rounds to loop over
+function playGame() {
+  let counter;
+  for (counter = 0; counter < 5; counter++) {
+    playRound();
+    if (counter === 4) {
+      if (humanScore > computerScore) {
+        console.log(`human won game with ${humanScore} points`);
+      } else if (computerScore > humanScore) {
+        console.log(`computer won game with ${computerScore} points`);
+      } else; //console.log("it's a tie!");
+    }
   }
-}
+  console.log(`humanScore: ${humanScore}, computerScore is: ${computerScore}`);
 
-playGame()
+}
+//runs game
+playGame();
